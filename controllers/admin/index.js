c9a0c547b0412ffecc2f0d75c5a688e3c55697bb
tianhua <azheng@SI-AZHENG.VCBrands.net>
2014-07-01 18:04:21 +0800

    admin auth

--- controllers/admin/index.js
+++ controllers/admin/index.js
@@ -8,7 +8,6 @@ module.exports = function (router) {
 
     var model = new AdminModel();
 
-
     router.get('/', function (req, res) {
         
         res.format({
@@ -20,5 +19,41 @@ module.exports = function (router) {
             }
         });
     });
+    
+    router.get('/users', function (req, res) {
+        
+        res.format({
+            json: function () {
+                res.json(model);
+            },
+            html: function () {
+                res.render('admin/index', model);
+            }
+        });
+    });
+    
+   router.get('/orders', function (req, res) {
+        
+        res.format({
+            json: function () {
+                res.json(model);
+            },
+            html: function () {
+                res.render('admin/index', model);
+            }
+        });
+    });
+   
+   router.get('/products', function (req, res) {
+       
+       res.format({
+           json: function () {
+               res.json(model);
+           },
+           html: function () {
+               res.render('admin/index', model);
+           }
+       });
+   });
 
 };
