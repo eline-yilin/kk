360f04e48c7c5b60b0584a7ccaa61d55bdf07add
tianhua <azheng@SI-AZHENG.VCBrands.net>
2014-07-16 16:30:36 +0800

    floor map page, change entity detail tabs,change home page button to point to floor map

---
+++ controllers/image/index.js
@@ -0,0 +1,27 @@
+'use strict';
+
+
+var ImageModel = require('../../models/image');
+
+
+module.exports = function (router) {
+
+    var model = new ImageModel();
+
+
+    router.get('/', function (req, res) {
+        var type = req.query.type;
+        if(type == 'map')
+        {
+        	var floors = [1,2,3,4,5];
+        	res.render('image/map', {name:'image',floors:floors});
+        }
+        else
+        {
+        	res.redirect('/');
+        }
+        
+        
+    });
+
+};
